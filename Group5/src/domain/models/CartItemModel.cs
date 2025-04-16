e082782798e53838de09968ff66562a0da0677e3
Leif Johannesson <56052518+lpjohannesson@users.noreply.github.com>
2025-04-16 14:48:08 -0400

    New cart models

--- Group5/src/domain/models/CartItemModel.cs
+++ Group5/src/domain/models/CartItemModel.cs
@@ -6,19 +6,7 @@ namespace Group5.src.domain.models
 {
     public class CartItem
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public int CartID { get; set; }
-        [Required]
-        public int ProductID { get; set; } 
-        [Required]
-        public int Quantity { get; set; }
-        [Column(TypeName = "decimal(18,2)")]
-        public double? Price { get; set; }
-
-        [JsonIgnore]
-        public Cart? Cart { get; set; }
         public Product? Product { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- Group5/src/domain/models/CartModel.cs
+++ Group5/src/domain/models/CartModel.cs
@@ -1,18 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Group5.src.domain.models
+﻿namespace Group5.src.domain.models
 {
     public class Cart
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string? UserId { get; set; }
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal TotalAmount { get; set; }
-
-        public User? User { get; set; }
-        public ICollection<CartItem>? CartItems { get; set; }
+        public List<CartItem> Items;
     }
 }
